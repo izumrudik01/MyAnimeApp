a93bfa6ebb2e4b8f9c55c1dc8907b8bf7c19b4d5
izumrudik01 <izumrudik01@yandex.ru>
2024-02-23 23:33:02 +0500

    auth

--- app/src/main/java/com/example/myanimeapp/AnimeActivity.kt
+++ app/src/main/java/com/example/myanimeapp/AnimeActivity.kt
@@ -1,12 +1,11 @@
 package com.example.myanimeapp
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import android.content.pm.ActivityInfo
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
-import com.example.myanimeapp.R.*
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myanimeapp.R.layout
+import org.jsoup.Jsoup
 
 class AnimeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,12 +13,78 @@ class AnimeActivity : AppCompatActivity() {
         setContentView(layout.activity_anime)
 
 
+        val url = "https://jut.su/anime"
+        val document = Jsoup.connect(url).get()
+        var selectedDiv = document.select("div.aaname")
+        var titles = mutableListOf<String>()
+        for (element in selectedDiv) {
+            titles.add(element.text())
+        }
 
-        val linkToProfile: ImageButton = findViewById(id.goToProfile)
-        linkToProfile.setOnClickListener {
+
+        val url1 = "https://jut.su/anime"
+        val document1 = Jsoup.connect(url1).get()
+            val imageElements = document1.select("image[href] style[src]").text()
+                println("Ссылка на изображение: ${imageElements.length}")
+        println("$imageElements")
+
+
+
+
+
+        println("Содержимое: ${titles}")
+        println("${titles.size}")
+
+
+
+        val linkToProfile: ImageButton = findViewById(R.id.goToProfile)
+        linkToProfile.setOnClickListener() {
             val intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
+
         }
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/myanimeapp/MainActivity.kt
+++ app/src/main/java/com/example/myanimeapp/MainActivity.kt
@@ -2,26 +2,36 @@ package com.example.myanimeapp
 
 import android.content.Intent
 import android.content.pm.ActivityInfo
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val policy = ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+
+
+
+
 
         val userLogin: EditText = findViewById(R.id.user_login)
         val userPassword: EditText = findViewById(R.id.user_password)
         val button: Button = findViewById(R.id.button_reg)
 
         button.setOnClickListener {
-            val login = userLogin.text.toString().trim()
+             val login = userLogin.text.toString().trim()
             val password = userPassword.text.toString().trim()
 
             if(login == "" || password == "")
@@ -44,3 +54,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+
+
+
+
+
+
+
